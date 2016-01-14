6cea61671a43ca24beadd4e42de1d03336ff99e7
7days <7days@PC-PEWBA141064>
2016-01-14 10:29:34 +0900

    commit

--- Java/my/manager/MyPropertyManager.java
+++ Java/my/manager/MyPropertyManager.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 /**
@@ -43,64 +44,15 @@ public enum MyPropertyManager {
     public String getValue(String key) {
         return prop.getProperty(key);
     }
+
+    /**
+     * デバッグログ出力
+     * @param level
+     */
+    public void debug(Level level) {
+        prop.stringPropertyNames().stream().sorted().forEach(key -> {
+            String val = String.format("%-25s", key) + " : " + prop.getProperty(key);
+            logger.log(level, val);
+        });
+    }
 }
-// package my.manager;
-//
-// import java.io.FileInputStream;
-// import java.io.IOException;
-// import java.util.Properties;
-//
-// import org.apache.log4j.Logger;
-//
-// /**
-// * PropertyManagerクラス
-// */
-// public class MyPropertyManager {
-//
-// /** Logger */
-// private static final Logger logger = Logger.getLogger(MyPropertyManager.class);
-//
-// /** 唯一のインスタンス */
-// private static final MyPropertyManager instance = new MyPropertyManager();
-// /** プロパティ */
-// private static Properties prop = null;
-//
-// /**
-// * コンストラクタ
-// */
-// private MyPropertyManager() {
-// }
-//
-// /**
-// * 唯一のインスタンスを取得します。
-// * @return 唯一のインスタンス
-// */
-// public static MyPropertyManager getInstance() {
-// return instance;
-// }
-//
-// /**
-// * プロパティを設定します。
-// * @param propertyFilePath プロパティファイル
-// */
-// synchronized public void setProperty(String propertyFilePath) {
-// if (prop != null) return;
-//
-// try (FileInputStream fis = new FileInputStream(propertyFilePath)) {
-// prop = new Properties();
-// prop.load(fis);
-// } catch (IOException e) {
-// // エラー自体は握り潰す
-// logger.error("property load error", e);
-// }
-// }
-//
-// /**
-// * プロパティから値を取得
-// * @param key
-// * @return keyに対応する値
-// */
-// public String getValue(String key) {
-// return prop.getProperty(key);
-// }
-// }
